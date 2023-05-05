e2b5377ecfb8bc4ab6372eff6c999f4939f3f8c0
Rahman Aziz <rahmanazizf@gmail.com>
2023-05-05 14:54:00 +0700

    perbaikan pada modul dtree perbaikan pada fungsi setter optimum_threshold

--- dtree.py
+++ dtree.py
@@ -11,18 +11,18 @@ import pandas as pd
 
 class DecisionTreeRegressor():
 
-    def __init__(self, data:pd.DataFrame, threshold_list:list) -> None:
+    def __init__(self, data:pd.DataFrame, threshold_list:list, optimum_threshold=None) -> None:
         assert isinstance(data, pd.DataFrame), f"data accepts DataFrame, got {type(data)}"
         assert isinstance(threshold_list, (list, tuple)), f"threshold_list accepts list or tuple, got {type(threshold_list)}"
         self.data = data
         self.threshold_list = threshold_list
-        self.optimum_threshold = None
+        self.optimum_threshold = optimum_threshold
         
     def split_data(self, data:pd.DataFrame, threshold_x:int) -> pd.DataFrame:
         """Split data into two regions"""
-        y_left = data[data['X'] < threshold_x]
-        y_right = data[data['X'] >= threshold_x]
-        return y_left, y_right
+        data_left = data[data['X'] < threshold_x]
+        data_right = data[data['X'] >= threshold_x]
+        return data_left, data_right
         
     def calc_avg(self, data:pd.DataFrame) -> float:
         """Calculate average -> predict data"""
@@ -39,10 +39,34 @@ class DecisionTreeRegressor():
         return self._optimum_threshold
     
     @optimum_threshold.setter
-    def optimum_threshold(self, data):
+    def optimum_threshold(self, value):
         """Find optimum threshold"""
-        opt_thr = 'cek'
-        self._optimum_threshold = opt_thr
+        rmsel_list = []
+        rmser_list = []
+        rmsetot_list = []
+
+        for thre in self.threshold_list:
+            data_left, data_right = self.split_data(self.data, thre)
+
+            if len(data_left)==0 or len(data_right)==0:
+                pass
+
+            # hitung nilai prediksi
+            pred_l = self.calc_avg(data_left['y'])
+            pred_r = self.calc_avg(data_right['y'])
+
+            rmse_l = self.calc_rmse(pred_l, data_left['y'])
+            rmse_r = self.calc_rmse(pred_r, data_right['y'])
+
+            rmsel_list.append(rmse_l)
+            rmser_list.append(rmse_r)
+            rmsetot_list.append(rmse_l+rmse_r)
+
+        rmsel_list = np.array(rmsel_list)
+        rmser_list = np.array(rmser_list)
+        rmsetot_list = np.array(rmsetot_list)
+        value = self.threshold_list[np.argmin(rmsetot_list)]
+        self._optimum_threshold = value
     
 
 def main():
